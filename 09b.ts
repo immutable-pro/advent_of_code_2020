c29c82387061df225bbacd47c57b5a888bbcd09e
Jorge Garcia Rodriguez <jorge@immutable.pro>
2020-12-14 16:50:31 +0000

    09 minor

--- 09b.ts
+++ 09b.ts
@@ -49,9 +49,6 @@ const findMinMax = (
 };
 
 const findWeakness = (lines: readonly number[], invalidNumber: number) => {
-  let min = Number.MAX_SAFE_INTEGER;
-  let max = Number.MIN_SAFE_INTEGER;
-
   for (let i = 0; i < lines.length; ) {
     let sum = lines[i];
 
